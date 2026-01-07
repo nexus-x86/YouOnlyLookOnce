a149f7f92799b3b7215859833483393f98c5d436
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2026-01-06 23:09:29 -0500

    minimal player tracker

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from utils import (read_video, 
                    save_video)
+from trackers import PlayerTracker
 
 def main():
+    # Read Video
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
 
-    save_video(video_frames, "output_videos/output_video.avi")
+    # Detect Players
+    player_tracker = PlayerTracker(model_path='yolov8x') 
+    player_detections = player_tracker.detect_frames(video_frames,
+                                                     read_from_stub=True,
+                                                     stub_path="tracker_stubs/player_detections.pkl")
+
+    # Draw output
+
+    ## draw player bounding boxes
+    output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+
+
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 if __name__ == "__main__":
     main()
